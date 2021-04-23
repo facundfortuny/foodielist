1a257207720c7e8551f19094e6042c275ec0e2c8
Facund Fortuny <fortuny@backcoder.com>
2021-04-23 11:41:08 +0200

    Change colours

--- src/components/Restaurants/RestaurantsMap.vue
+++ src/components/Restaurants/RestaurantsMap.vue
@@ -15,8 +15,8 @@
         :shape="shape"
         :icon="
           marker.visited
-            ? 'https://maps.google.com/mapfiles/ms/micons/yellow.png'
-            : 'https://maps.google.com/mapfiles/ms/icons/red.png'
+            ? 'https://maps.google.com/mapfiles/ms/icons/blue.png'
+            : 'https://maps.google.com/mapfiles/ms/icons/yellow.png'
         "
         @click="openInfo(marker, index)"
         @dblclick="openPage(marker)"
