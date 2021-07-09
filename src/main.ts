de56802edc9db64680a331bdc6cd1b9e77f81cd3
Facund Fortuny <fortuny@backcoder.com>
2021-07-09 19:38:00 +0200

    add google tag manager

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import store from './store';
 import * as GmapVue from 'gmap-vue';
 import firebase from 'firebase/app';
 import 'firebase/analytics';
+import VueGtm from '@gtm-support/vue2-gtm';
 
 Vue.use(GmapVue, {
   load: {
@@ -20,6 +21,16 @@ Vue.use(GmapVue, {
 Vue.config.productionTip = false;
 
 firebase.analytics();
+
+Vue.use(VueGtm, {
+  id: 'GTM-5BXXD3H',
+  defer: false,
+  enabled: process.env.NODE_ENV === 'production',
+  debug: process.env.NODE_ENV !== 'production',
+  loadScript: true,
+  vueRouter: router,
+  trackOnNextTick: false,
+});
 
 new Vue({
   router,
