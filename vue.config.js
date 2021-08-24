bced031207b78700bf8186d003ddc41bb3d72bf2
Facund Fortuny <fortuny@backcoder.com>
2021-08-24 17:57:47 +0200

    Enable PWA

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
+  pwa: {
+    name: 'Foodie',
+    themeColor: '#FECD52',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    },
+  },
 };
